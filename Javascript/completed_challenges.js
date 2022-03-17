e30076c9b45580e841ad47e70df8622e84091188
calcala12 <caalcala123@gmail.com>
2022-03-17 13:12:16 -0700

    Create completed_challenges.js

---
+++ Javascript/completed_challenges.js
@@ -0,0 +1,10 @@
+//Multiply
+function multiply(a,b){
+ return a * b;
+}
+
+//Convert a String to a Number!
+var stringToNumber = function(str){
+  //convert to Integer via parseInt
+  return parseInt(str);
+}
