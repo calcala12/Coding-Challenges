054eb9379af6eca3427ffc02fd7a4c47c226d4a7
calcala12 <caalcala123@gmail.com>
2022-03-21 19:53:51 -0700

    Update completed_challenges.js

--- Javascript/completed_challenges.js
+++ Javascript/completed_challenges.js
@@ -8,3 +8,10 @@ var stringToNumber = function(str){
   //convert to Integer via parseInt
   return parseInt(str);
 }
+
+//Square and Sum Numbers
+function squareSum(numbers){
+  return numbers.reduce(function(sum, n){
+    return (n*n) + sum;
+  }, 0)
+}
