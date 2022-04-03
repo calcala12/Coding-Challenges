1c243f4f3db547b8e537fcecf924c0b5723611c5
calcala12 <caalcala123@gmail.com>
2022-04-02 19:17:53 -0700

    Update completed_challenges.js

--- Javascript/completed_challenges.js
+++ Javascript/completed_challenges.js
@@ -49,3 +49,12 @@ function makeNegative(num) {
 function greet(name) {
   return "Hello, " + name + " how are you doing today?";
 }
+
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+var summation = function (num) {
+ let sum = 0
+ for (let i = 0; i <= num; i++) {
+   sum += i
+ }
+ return sum
+}
