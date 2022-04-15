dae678c28732a1a9fa7232fa62876eda2c3abb40
calcala12 <caalcala123@gmail.com>
2022-04-14 21:01:56 -0700

    Update completed_challenges.js

--- Javascript/completed_challenges.js
+++ Javascript/completed_challenges.js
@@ -91,3 +91,15 @@ function abbrevName(name){
 function noSpace(x){
   return x.replace(/\s/g, '');
   }
+
+  /* Return the number (count) of vowels in the given string.
+  We will consider a, e, i, o, u as vowels for this Kata (but not y).
+  The input string will only consist of lower case letters and/or spaces. */
+
+  function getCount(str) {
+    var m = 0;
+    var m = str.match(/[aeiou]/gi);
+    return m === null ? 0 : m.length;
+  }
+    
+   
