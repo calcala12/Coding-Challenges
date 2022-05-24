75d856e2bb75b4a45f3843ebabbdf81b08b6dbe9
calcala12 <caalcala123@gmail.com>
2022-05-23 18:09:10 -0700

    Update completed_challenges.js

--- Javascript/completed_challenges.js
+++ Javascript/completed_challenges.js
@@ -128,3 +128,21 @@ function stringClean(s){
   const noNumbers = s.replace(/[0-9]/g,'')
   return noNumbers
 }
+
+// 7Kyu - Testing 1-2-3 Solutions:
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples:
+// number([]) // => []
+// number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
+
+var number = function(array){
+  return array.map(function(line,index){
+    return(index + 1) + ":" + line;
+  });
+}
