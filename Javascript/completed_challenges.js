ca714c059a0b41c546e2170fb8d74eeebb833775
calcala12 <caalcala123@gmail.com>
2022-06-03 21:53:35 -0700

    Update completed_challenges.js

--- Javascript/completed_challenges.js
+++ Javascript/completed_challenges.js
@@ -198,3 +198,11 @@ class SmallestIntegerFinder {
 function removeChar(str) {
   return str.slice(1, -1);
 }
+
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+const century = year => Math.ceil(year/100)
